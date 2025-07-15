f853ff341c2730deabeb753a8caf733507385381
soterri <thach22204@gmail.com>
2025-07-15 16:03:24 -0400

    -

---
+++ src/com/syntax/utils/BaseClass.java
@@ -0,0 +1,38 @@
+package com.syntax.utils;
+
+import javax.management.RuntimeErrorException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+
+	public void setUp() {
+
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+		
+		switch(ConfigsReader.getProperty("browser").toLowerCase()) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
+			WebDriver driver = new ChromeDriver();
+			break;
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", Constants.GECKO_DRIVER_PATH);
+			WebDriver driver1 = new FirefoxDriver();
+			break;
+			default:
+				throw new RuntimeException("Browser is not supported");
+		}
+		
+		driver.get(ConfigsReader.getProperty("url"));
+	}
+	public static void tearDown() {
+		if(driver!=null) {
+			driver.quit();
+		}
+	}
+	
+}
--- src/com/syntax/utils/ConfigsReader.java
+++ src/com/syntax/utils/ConfigsReader.java
@@ -1,5 +1,39 @@
 package com.syntax.utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigsReader {
 
+	public static Properties prop;
+	/**
+	 * This method will read properties file
+	 * 
+	 * @param filePath
+	 */
+	public static void readProperties(String filePath) {
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			prop = new Properties();
+			prop.load(fis);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+/**
+ * this method will return value at specified key
+ * @param String key
+ * @return String 
+ */
+	public static String getProperty(String key) {
+		
+		return prop.getProperty(key);
+	}
+	
 }
